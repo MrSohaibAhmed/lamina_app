0edd1eb9140ce36f612f15c937bc72c8320efb3d
sohaibahmed <sohaibahmed248@gmail.com>
2024-05-18 20:05:25 +0500

    updated connection

--- src/components/Dashboard/DashBoardComponents/HoldingTable.jsx
+++ src/components/Dashboard/DashBoardComponents/HoldingTable.jsx
@@ -1,80 +1,106 @@
-import React, { useEffect, useState } from 'react'
-import arrowImg from '../../../assets/dashboard/arrows.png';
-import solIconImg from '../../../assets/dashboard/sol-icon.png';
-import transationTableIconImg from '../../../assets/dashboard/transaction-table-icon.png'
-import { setHoldings } from '../../hooks/useTransactions';
+import React, { useEffect, useState } from "react";
+import arrowImg from "../../../assets/dashboard/arrows.png";
+import solIconImg from "../../../assets/dashboard/sol-icon.png";
+import transationTableIconImg from "../../../assets/dashboard/transaction-table-icon.png";
+import { setHoldings } from "../../hooks/useTransactions";
 const HoldingTable = ({ address }) => {
+  const [holdings, setHoldingsData] = useState([]);
 
-    const [holdings, setHoldingsData] = useState([])
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const holdingsData = await setHoldings();
-                setHoldingsData(holdingsData.items);
-                console.log(holdingsData.items, " these are holdings");
-
-            } catch (error) {
-                console.error('Error fetching holdings:', error);
-            }
-        };
-
-        fetchData();
-    }, [address]);
-
-    const formatTxHash = (txHash) => {
-        if (!txHash) return '';
-        const start = txHash.slice(0, 5);
-        const end = txHash.slice(-5);
-        return `${start}.....${end}`;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const holdingsData = await setHoldings(
+          localStorage.getItem("publicKey")
+        );
+        setHoldingsData(holdingsData.items);
+        console.log(holdingsData.items, " these are holdings");
+      } catch (error) {
+        console.error("Error fetching holdings:", error);
+      }
     };
 
-    return (
-        <>
-            <div className="table-responsive">
-                <table className="table table-dark">
-                    <thead>
-                        <tr className="border-0">
-                            <th scope="col"> Logo </th>
-                            <th scope="col"> Name </th>
-                            <th scope="col"> Symbol </th>
-                            <th scope="col">Account Address</th>
-                            <th scope="col">Price Usd</th>
-                            {/* <th scope="col">Mint Address</th> */}
-                            <th scope="col">Balance</th>
-                            <th scope="col">Amount</th>
-                            <th scope="col">ChainId</th>
+    fetchData();
+  }, [address]);
 
-                        </tr>
-                    </thead>
-                    <tbody className="border-top-0">
-                        {
-                            holdings.map((holding, index) =>(
-                                holding.address !== 'So11111111111111111111111111111111111111111' ?( 
-                                    <tr key={index} className=' align-items-center '>
-                                        <td style={{verticalAlign:"middle"}}><img className='rounded-circle' height="40"  width= "40" src={holding.logoURI}/></td>
-                                        <td style={{verticalAlign:"middle"}}>{holding.name}</td>
-                                        <td style={{verticalAlign:"middle"}}>{holding.symbol}</td>
-                                        {/* <td>SOL STREET BETS</td> */}
-                                        {/* <td>{holding.address}</td> */}
-                                        <td style={{verticalAlign:"middle"}}>{formatTxHash(holding.address)}</td>
-                                        {/* <td> 3XUv67...7uU8</td> */}
-                                        <td style={{verticalAlign:"middle"}}>${holding.priceUsd}</td>
-                                        <td style={{verticalAlign:"middle"}}>${holding.balance}</td>
-                                        <td style={{verticalAlign:"middle"}}>{holding.uiAmount/1000}K</td>
-                                        <td style={{verticalAlign:"middle"}}><img src={solIconImg} width="18px" alt="" /> {holding.chainId}</td>
-                                    </tr>):null
+  const formatTxHash = (txHash) => {
+    if (!txHash) return "";
+    const start = txHash.slice(0, 5);
+    const end = txHash.slice(-5);
+    return `${start}.....${end}`;
+  };
 
-                                )
-                            )
-                        }
+  return (
+    <>
+      <div className="table-responsive">
+        <table className="table table-dark">
+          <thead>
+            <tr className="border-0">
+              <th scope="col"> Logo </th>
+              <th scope="col"> Name </th>
+              <th scope="col"> Symbol </th>
+              <th scope="col">Account Address</th>
+              <th scope="col">Price Usd</th>
+              {/* <th scope="col">Mint Address</th> */}
+              <th scope="col">Balance</th>
+              <th scope="col">Amount</th>
+              <th scope="col">ChainId</th>
+            </tr>
+          </thead>
+          <tbody className="border-top-0">
+            {holdings.length === 0 ? (
+              <tr>
+                <td
+                  colSpan="8"
+                  style={{ textAlign: "center", verticalAlign: "middle" }}
+                >
+                  No holdings available
+                </td>
+              </tr>
+            ) : (
+              holdings.map((holding, index) =>
+                holding.address !==
+                "So11111111111111111111111111111111111111111" ? (
+                  <tr key={index} className="align-items-center">
+                    <td style={{ verticalAlign: "middle" }}>
+                      <img
+                        className="rounded-circle"
+                        height="40"
+                        width="40"
+                        src={holding.logoURI}
+                      />
+                    </td>
+                    <td style={{ verticalAlign: "middle" }}>{holding.name}</td>
+                    <td style={{ verticalAlign: "middle" }}>
+                      {holding.symbol}
+                    </td>
+                    {/* <td>SOL STREET BETS</td> */}
+                    {/* <td>{holding.address}</td> */}
+                    <td style={{ verticalAlign: "middle" }}>
+                      {formatTxHash(holding.address)}
+                    </td>
+                    {/* <td> 3XUv67...7uU8</td> */}
+                    <td style={{ verticalAlign: "middle" }}>
+                      ${holding.priceUsd}
+                    </td>
+                    <td style={{ verticalAlign: "middle" }}>
+                      ${holding.balance}
+                    </td>
+                    <td style={{ verticalAlign: "middle" }}>
+                      {holding.uiAmount / 1000}K
+                    </td>
+                    <td style={{ verticalAlign: "middle" }}>
+                      <img src={solIconImg} width="18px" alt="" />{" "}
+                      {holding.chainId}
+                    </td>
+                  </tr>
+                ) : null
+              )
+            )}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
 
-
-                    </tbody>
-                </table>
-            </div>
-        </>
-    )
-}
-
-export default HoldingTable
+export default HoldingTable;
--- src/components/hooks/usePhantom.js
+++ src/components/hooks/usePhantom.js
@@ -8,26 +8,17 @@ const usePhantom = () => {
     const [web3, setWeb3] = useState(null);
     const [signedMessage, setSignedMessage] = useState(null);
     const [solanaKey, setSolanaKey] = useState(null);
-
     const connectToPhantom = async () => {
-        if (window.ethereum) {
+        if (window.solana) { // Check if Solana wallet extension is available
             try {
-                await window.ethereum.request({ method: 'eth_requestAccounts' });
-                const web3Instance = new Web3(window.ethereum);
-                const accounts = await web3Instance.eth.getAccounts();
+                // Request connection to Solana wallet
                 await window.solana.connect();
                 const solanaPublicKey = window.solana.publicKey.toString();
-                console.log(accounts, solanaPublicKey, ">>>>>>>");
-                setAccount(accounts[0]);
-                setWeb3(web3Instance);
+                console.log(solanaPublicKey, ">>>>>>>");
                 setSolanaKey(solanaPublicKey);
                 setConnected(true);
-                localStorage.setItem("account", accounts[0]);
-                localStorage.setItem("web3", web3Instance);
-                localStorage.setItem("connected", true);
                 localStorage.setItem("solanaKey", solanaPublicKey);
-
-
+                localStorage.setItem("connected", true);
             } catch (error) {
                 console.error(error);
             }
@@ -35,6 +26,32 @@ const usePhantom = () => {
             alert('Phantom extension not detected!');
         }
     };
+    // const connectToPhantom = async () => {
+    //     if (window.ethereum) {
+    //         try {
+    //             await window.ethereum.request({ method: 'eth_requestAccounts' });
+    //             const web3Instance = new Web3(window.ethereum);
+    //             const accounts = await web3Instance.eth.getAccounts();
+    //             await window.solana.connect();
+    //             const solanaPublicKey = window.solana.publicKey.toString();
+    //             console.log(accounts, solanaPublicKey, ">>>>>>>");
+    //             setAccount(accounts[0]);
+    //             setWeb3(web3Instance);
+    //             setSolanaKey(solanaPublicKey);
+    //             setConnected(true);
+    //             localStorage.setItem("account", accounts[0]);
+    //             localStorage.setItem("web3", web3Instance);
+    //             localStorage.setItem("connected", true);
+    //             localStorage.setItem("solanaKey", solanaPublicKey);
+
+
+    //         } catch (error) {
+    //             console.error(error);
+    //         }
+    //     } else {
+    //         alert('Phantom extension not detected!');
+    //     }
+    // };
     const disconnectFromMetaMask = () => {
         setConnected(false);
         setAccount(null);
@@ -47,46 +64,92 @@ const usePhantom = () => {
         localStorage.removeItem("-walletlink:https://www.walletlink.org:EIP6963ProviderUUID");
 
     };
+    // const newUsersignMessage = async () => {
+    //     if (web3 && account) {
+    //         const message = 'To avoid digital dognappers, sign below to authenticate with fusiond_app.';
+    //         const encoder = new TextEncoder();
+    //         const msg = `0x${Array.prototype.map.call(encoder.encode(message), x => ('00' + x.toString(16)).slice(-2)).join('')}`;
+    //         try {
+    //             const sign = await web3.eth.personal.sign(msg, account, 'Example password');
+    //             setSignedMessage(sign);
+    //             localStorage.setItem("sign", sign);
+
+    //         } catch (error) {
+    //             console.error('Error signing message:', error);
+    //         }
+    //         navi("/step1");
+    //     } else {
+    //         console.error('Web3 instance or account not available.');
+    //     }
+    // };
     const newUsersignMessage = async () => {
-        if (web3 && account) {
+        debugger
+        if (window.solana && solanaKey) {
             const message = 'To avoid digital dognappers, sign below to authenticate with fusiond_app.';
             const encoder = new TextEncoder();
-            const msg = `0x${Array.prototype.map.call(encoder.encode(message), x => ('00' + x.toString(16)).slice(-2)).join('')}`;
-            try {
-                const sign = await web3.eth.personal.sign(msg, account, 'Example password');
-                setSignedMessage(sign);
-                localStorage.setItem("sign", sign);
+            const encodedMessage = encoder.encode(message);
 
+            try {
+                const signedMessage = await window.solana.signMessage(encodedMessage, 'utf8');
+                // const signature = Buffer.from(signedMessage.signature).toString('hex'); // Convert signature to hex string
+                // setSignedMessage(signature);
+                // localStorage.setItem("sign", signature);
+                debugger
+                navi("/step1");
             } catch (error) {
                 console.error('Error signing message:', error);
             }
-            navi("/step1");
         } else {
-            console.error('Web3 instance or account not available.');
+            console.error('Solana wallet instance or public key not available.');
         }
     };
+
+    // const signMessage = async () => {
+    //     if (web3 && account) {
+    //         const message = 'To avoid digital dognappers, sign below to authenticate with fusiond_app.';
+    //         const encoder = new TextEncoder();
+    //         const msg = `0x${Array.prototype.map.call(encoder.encode(message), x => ('00' + x.toString(16)).slice(-2)).join('')}`;
+    //         try {
+    //             const sign = await web3.eth.personal.sign(msg, account, 'Example password');
+    //             setSignedMessage(sign);
+    //             localStorage.setItem("sign", sign);
+
+    //         } catch (error) {
+    //             console.error('Error signing message:', error);
+    //         }
+    //         // debugger
+    //         navi("/dashboard");
+    //     } else {
+    //         console.error('Web3 instance or account not available.');
+    //     }
+    // };
     const signMessage = async () => {
-        if (web3 && account) {
+        debugger
+        if (window.solana && solanaKey) {
             const message = 'To avoid digital dognappers, sign below to authenticate with fusiond_app.';
             const encoder = new TextEncoder();
-            const msg = `0x${Array.prototype.map.call(encoder.encode(message), x => ('00' + x.toString(16)).slice(-2)).join('')}`;
-            try {
-                const sign = await web3.eth.personal.sign(msg, account, 'Example password');
-                setSignedMessage(sign);
-                localStorage.setItem("sign", sign);
+            const encodedMessage = encoder.encode(message);
 
+            try {
+                const signedMessage = await window.solana.signMessage(encodedMessage, 'utf8');
+                const signature = signedMessage.signature.toString('hex'); // Convert signature to hex string
+                setSignedMessage(signature);
+                localStorage.setItem("sign", signature);
+
+                navi("/dashboard");
             } catch (error) {
                 console.error('Error signing message:', error);
             }
-            // debugger
-            navi("/dashboard");
         } else {
-            console.error('Web3 instance or account not available.');
+            console.error('Solana wallet instance or public key not available.');
         }
     };
 
 
-    return { connectToPhantom, disconnectFromMetaMask, connected, account, web3, signMessage, newUsersignMessage, solanaKey };
+
+    // return { connectToPhantom, disconnectFromMetaMask, connected, account, web3, signMessage, newUsersignMessage, solanaKey };
+    return { connectToPhantom, disconnectFromMetaMask, connected, signMessage, newUsersignMessage, solanaKey };
+
 };
 
 export default usePhantom;
