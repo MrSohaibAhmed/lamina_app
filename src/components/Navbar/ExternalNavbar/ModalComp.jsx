51b8e8e99eddd4148b5ee14ba3dfb40fc1da66f6
Fida-khaplu <fidakhaplu@gmail.com>
2024-05-23 12:09:56 +0500

    fix the modal issues

--- src/components/Navbar/ExternalNavbar/ModalComp.jsx
+++ src/components/Navbar/ExternalNavbar/ModalComp.jsx
@@ -4,9 +4,36 @@ import solfareImg from "../../../assets/navbarImg/solfareimg.webp";
 import phantomImg from "../../../assets/navbarImg/phantom.webp";
 import usePhantom from "../../hooks/usePhantom";
 import { checkUser } from "../../hooks/useWallet";
-import './navbar.css'
+import './Navbar.css'
 
 const ModalComp = ({ connectToPhantom, connectToSolflare }) => {
+
+  const handleConnect = (value) => {
+
+    if(value==='solfare'){
+
+      connectToSolflare();
+    }
+    if(value==='phantom'){
+      connectToPhantom()
+    }
+    const modalElement = document.getElementById('staticBackdrop');
+    const modalBackdrop = document.querySelector('.modal-backdrop');
+    document.body.classList.add('overflow-auto');
+
+    if (modalElement) {
+      const modal = bootstrap.Modal.getInstance(modalElement);
+      if (modal) {
+        modal.hide();
+      }
+    }
+    if (modalBackdrop) {
+      modalBackdrop.remove();
+    }
+  };
+
+
+
   return (
     <>
       {/* Button trigger modal */}
@@ -55,7 +82,7 @@ const ModalComp = ({ connectToPhantom, connectToSolflare }) => {
               ></button>
             </div>
             <div className="modal-body">
-              <div onClick={connectToSolflare} className="m-2 d-flex align-items-center px-2" style={{ width: "260px" , backgroundColor:"#2e2e55" , borderRadius:"5px"}}>
+              <div onClick={()=>handleConnect('solfare')} className="m-2 d-flex align-items-center px-2" data-bs-dismiss="modal" style={{ width: "260px" , backgroundColor:"#2e2e55" , borderRadius:"5px"}}>
                 <span>
                   <img src={solfareImg} />
                 </span>
@@ -63,7 +90,7 @@ const ModalComp = ({ connectToPhantom, connectToSolflare }) => {
                   Connect to Solfare
                 </button>
               </div>
-              <div onClick={connectToPhantom} className="m-2 d-flex align-items-center px-2 " style={{ width: "260px" , backgroundColor:"#2e2e55" , borderRadius:"5px"}}>
+              <div onClick={()=>handleConnect('phantom')} className="m-2 d-flex align-items-center px-2 " data-bs-dismiss style={{ width: "260px" , backgroundColor:"#2e2e55" , borderRadius:"5px"}}>
                 <span>
                   <img src={phantomImg} />
                 </span>
