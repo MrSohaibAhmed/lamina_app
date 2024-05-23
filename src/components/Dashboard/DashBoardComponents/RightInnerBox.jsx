d027d169cf32ece9caa304cec75379406f8f1ca4
sohaibahmed <sohaibahmed248@gmail.com>
2024-05-23 15:35:23 +0500

    update

--- src/components/Dashboard/DashBoardComponents/RightInnerBox.jsx
+++ src/components/Dashboard/DashBoardComponents/RightInnerBox.jsx
@@ -39,7 +39,7 @@ const RightInnerBox = ({ data, checkZoomLevel, solBalance }) => {
   const [sellVol, setsellVol] = useState(0);
   const [activeButton, setActiveButton] = useState(null);
   const [inputAmountVal, setInputAmountVal] = useState();
-  const[buttonValue , setButtonValue]= useState(null)
+  const [buttonValue, setButtonValue] = useState(null);
 
   const scrollToTop = () => {
     scroll.scrollToTop();
@@ -150,7 +150,7 @@ const RightInnerBox = ({ data, checkZoomLevel, solBalance }) => {
     // debugger
     console.log("solona balance is =>>", solBalance);
     if (selectedValue !== null && selectedValue !== 0) {
-      if (solBalance !==0) {
+      if (solBalance !== 0) {
         const value = {
           address: localStorage.getItem("publicKey"),
           amount: selectedValue * 1000000000,
@@ -193,16 +193,13 @@ const RightInnerBox = ({ data, checkZoomLevel, solBalance }) => {
           success: "",
           error: "",
         });
+      } else {
+        toast.error("You Donot have Enough Sol Balance");
       }
-      else{
-        toast.error("you donot have enough sol balance")
-      }
-      
     } else {
-      
       console.error("No value selected");
       // noValueError();
-      toast.error("pls select the value")
+      toast.error("Please select a value First");
     }
   };
 
