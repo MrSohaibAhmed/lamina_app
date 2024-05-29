5acc9f91053a0165366c8d08555363d21d9a35b8
MrSohaibAhmed <129491757+MrSohaibAhmed@users.noreply.github.com>
2024-05-28 20:57:11 -0700

    update

--- src/components/LuminaWallet/Step3.jsx
+++ src/components/LuminaWallet/Step3.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import '../Navbar/ExternalNavbar/Navbar.css'
 import { useNavigate } from 'react-router-dom';
 import KeyContext from '../../context/walletContext';
-
+import { useContext } from 'react';
 const Step3 = () => {
     const { privateKey, publicKey, decryptPrivateKey } = useContext(KeyContext);
 
