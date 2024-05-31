53f60152b36d1ffd49cf8e0b8fb652a550d07468
sohaibahmed <sohaibahmed248@gmail.com>
2024-05-31 14:43:46 +0500

    update

--- src/components/Dashboard/DashBoardComponents/RightAccordian.jsx
+++ src/components/Dashboard/DashBoardComponents/RightAccordian.jsx
@@ -1,77 +1,127 @@
-import React, { useEffect } from 'react'
-import '../Dashboard.css'
+import React, { useEffect, useState } from "react";
+import "../Dashboard.css";
+import { setDataAndSecurity } from "../../hooks/useTransactions";
 
-const RightAccordian = () => {
-   
-    return (
-        <div className="accordion accordion-flush bg-dark" id="accordionFlush">
-            <div className="accordion-item">
-                <h2 className="accordion-header" id="flush-headingtwo">
-                    <button className="accordion-button accordion-button-inner collapsed" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#flush-collapsetwo" aria-expanded="false" aria-controls="flush-collapseOne">
-                        Data & security
-                    </button>
-                </h2>
-                <div id="flush-collapsetwo" className="accordion-collapse accordion-body-inner collapse" aria-labelledby="flush-headingOne"
-                    data-bs-parent="#accordionFlush">
-                    <div className="accordion-body data-table"> <table className="table">
-                        <tbody>
-                            <tr>
-                                <td> Mint Authority
-                                </td>
-                                <td className="text-end text-danger"><i className="fa fa-exclamation-circle" aria-hidden="true"></i> Enabled
-                                </td>
-                            </tr>
-                            <tr>
-                                <td> Freeze Authority
-                                </td>
-                                <td className="text-end text-success">Disabled
+const RightAccordian = ({ address }) => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await setDataAndSecurity(
+          address.pairs[0].baseToken.address
+        );
+        setData(response);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-                                </td>
-                            </tr>
-                            <tr>
-                                <td> Pooled Ansem Dog
-                                </td>
-                                <td className="text-end text-danger"><i className="fa fa-exclamation-circle" aria-hidden="true"></i> 0.00%
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>  LP Burned
-                                </td>
-                                <td className="text-end"> $0.0₄
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>   Pooled SOL
-                                </td>
-                                <td className="text-end"> $0.17
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>  Top 10 Holders
-                                </td>
-                                <td className="text-end text-danger"><i className="fa fa-exclamation-circle" aria-hidden="true"></i> 99.47%
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>  Deployer
-                                </td>
-                                <td className="text-end"><a href="#"> Bq7..CnPm</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td> Open Trading
-                                </td>
-                                <td className="text-end"> May 7 15:44:25
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    </div>
-                </div>
-            </div>
+    fetchData();
+  }, [address]);
+  const truncateAddress = (address) => {
+    if (!address) return "";
+    return `${address.slice(0, 3)}...${address.slice(-3)}`;
+  };
+  function epochToDateTime(epochTime) {
+    const epochMilliseconds = epochTime * 1000;
+    const date = new Date(epochMilliseconds);
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const day = date.getDate().toString().padStart(2, "0");
+    const hours = date.getHours().toString().padStart(2, "0");
+    const minutes = date.getMinutes().toString().padStart(2, "0");
+    const seconds = date.getSeconds().toString().padStart(2, "0");
+    const standardTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    return standardTime;
+  }
+  return (
+    <div className="accordion accordion-flush bg-dark" id="accordionFlush">
+      <div className="accordion-item">
+        <h2 className="accordion-header" id="flush-headingtwo">
+          <button
+            className="accordion-button accordion-button-inner collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#flush-collapsetwo"
+            aria-expanded="false"
+            aria-controls="flush-collapseOne"
+          >
+            Data & security
+          </button>
+        </h2>
+        <div
+          id="flush-collapsetwo"
+          className="accordion-collapse accordion-body-inner collapse"
+          aria-labelledby="flush-headingOne"
+          data-bs-parent="#accordionFlush"
+        >
+          <div className="accordion-body data-table">
+            {" "}
+            <table className="table">
+              <tbody>
+                <tr>
+                  <td> Mint Authority</td>
+                  <td className="text-end text-danger">
+                    <i
+                      className="fa fa-exclamation-circle"
+                      aria-hidden="true"
+                    ></i>{" "}
+                    {data?.mintSlot ? "Enabled" : "Disabled"}
+                  </td>
+                </tr>
+                <tr>
+                  <td> Freeze Authority</td>
+                  <td className="text-end text-success">
+                    {data?.freezeAuthority ? data.freezeAuthority : "Disabled"}
+                  </td>
+                </tr>
+                <tr>
+                  <td> Pooled Ansem Dog</td>
+                  <td className="text-end text-danger">
+                    <i
+                      className="fa fa-exclamation-circle"
+                      aria-hidden="true"
+                    ></i>{" "}
+                    0.00%
+                  </td>
+                </tr>
+                <tr>
+                  <td> LP Burned</td>
+                  <td className="text-end"> $0.0₄</td>
+                </tr>
+                <tr>
+                  <td> Pooled SOL</td>
+                  <td className="text-end"> $0.17</td>
+                </tr>
+                <tr>
+                  <td> Top 10 Holders</td>
+                  <td className="text-end text-danger">
+                    <i
+                      className="fa fa-exclamation-circle"
+                      aria-hidden="true"
+                    ></i>{" "}
+                    {(data?.top10HolderPercent * 100).toFixed(2)}%
+                  </td>
+                </tr>
+                <tr>
+                  <td> Deployer</td>
+                  <td className="text-end">
+                    <a href="#">{truncateAddress(data.creatorAddress)}</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td> Open Trading</td>
+                  <td className="text-end">
+                    {epochToDateTime(data?.creationTime)}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default RightAccordian
+export default RightAccordian;
