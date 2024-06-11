986062b3f6c027ad05ed415c0f1acee435ea16fd
Fida Hussain <136558587+Fida-khaplu@users.noreply.github.com>
2024-06-11 16:34:36 +0500

    updated newPair table

--- src/components/NewPairs/index.jsx
+++ src/components/NewPairs/index.jsx
@@ -9,7 +9,14 @@ function Newpairs() {
     return (
         <>
         <InternalNavbar/>
-        <div className=' container-fluid'>
+        <div className=' container-fluid px-5'>
+            <div className=' py-5'>
+                <div>
+                    <h4 className=' font-weight-bold'>NEW PAIRS</h4>
+                    <p>New token pairs in the last 24-hours updated in real-time.</p>
+                </div>
+                <div></div>
+            </div>
         <NewpairTable/>
         </div>
         </>
